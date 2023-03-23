5634cd1e67f3f0f8792352257e7128b4b0eaa618
Steadystudy <sgmin206@naver.com>
2023-03-23 15:13:08 +0900

    Fix: Link key id 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,9 @@
 'use client';
 
 import Link from 'next/link';
-import { useId } from 'react';
 
 export default function Header() {
   const pages = ['posts', 'about', 'contact'];
-  const id = useId();
 
   return (
     <header className="flex flex-row justify-between bg-lime-200 text-black font-[700]">
@@ -13,8 +11,8 @@ export default function Header() {
         <h1 className="text-2xl ml-4">{"MSG's Blog"}</h1>
       </Link>
       <nav className="flex flex-row text-1xl items-center">
-        {pages.map((page) => (
-          <Link key={id} href={`/${page}`} className="mr-4 text-center">
+        {pages.map((page, i) => (
+          <Link key={i} href={`/${page}`} className="mr-4 text-center">
             {page}
           </Link>
         ))}
