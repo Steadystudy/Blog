86ad223693786803eae74b0bbac13e41b06f70f1
Steadystudy <sgmin206@naver.com>
2023-03-25 22:24:00 +0900

    Feat: posts 페이지

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,13 @@
-import React from 'react';
+import FilteredPosts from '@/components/FilteredPosts';
+import { getAllPosts } from '@/service/posts';
 
-export default function PostsPage() {
-  return <div>PostsPage</div>;
+export default async function PostsPage() {
+  const posts = await getAllPosts();
+  const categories = [...new Set(posts.map((post) => post.category))];
+
+  return (
+    <section>
+      <FilteredPosts posts={posts} categories={categories} />
+    </section>
+  );
 }
--- src/components/PostGrid.tsx
+++ src/components/PostGrid.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export default function PostGrid({ posts }: Props) {
   return (
-    <ul className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+    <ul className="grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4">
       {posts.map((post) => (
         <li key={post.id}>{<PostCard post={post} />}</li>
       ))}
