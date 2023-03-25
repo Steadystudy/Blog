279b4b30e2910f3acaea00bd65a15e1df7542fe7
Steadystudy <sgmin206@naver.com>
2023-03-25 22:23:39 +0900

    Feat: posts 페이지 FilteredPosts Component

---
+++ src/components/FilteredPosts.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { Post } from '@/service/posts';
+import { useState } from 'react';
+import PostGrid from './PostGrid';
+import Categories from './Categories';
+
+interface Props {
+  posts: Post[];
+  categories: string[];
+}
+
+const ALL_POSTS = 'All Posts';
+
+export default function FilteredPosts({ posts, categories }: Props) {
+  const [selected, setSelected] = useState(ALL_POSTS);
+  const filtered =
+    selected === ALL_POSTS ? posts : posts.filter((post) => post.category === selected);
+
+  return (
+    <section className="flex m-4">
+      <PostGrid posts={filtered} />
+      <Categories
+        categories={[ALL_POSTS, ...categories]}
+        onClick={setSelected}
+        selected={selected}
+      />
+    </section>
+  );
+}
