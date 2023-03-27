f9f4ebc0f83f76790ffa913aad3dce88ed1171e6
Steadystudy <sgmin206@naver.com>
2023-03-27 15:43:51 +0900

    Feat: posts/[slug] 페이지 (각 포스트 페이지)

--- src/app/posts/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-interface Props {
-  params: {
-    id: string;
-  };
-}
-
-export default function PostPage({ params }: Props) {
-  return <div>{params.id}</div>;
-}
---
+++ src/app/posts/[slug]/page.tsx
@@ -0,0 +1,32 @@
+import MarkdownViewer from '@/components/MarkdownViewer';
+import { getPostData } from '@/service/posts';
+import Image from 'next/image';
+
+interface Props {
+  params: {
+    slug: string;
+  };
+}
+
+export default async function PostPage({ params: { slug } }: Props) {
+  const { title, content, date, description, path } = await getPostData(slug);
+
+  return (
+    <article className="mx-8">
+      <Image
+        className="w-full overflow-hidden max-h-96"
+        src={`/images/posts/${path}.png`}
+        alt={title}
+        width={500}
+        height={400}
+      />
+      <section className="flex flex-col">
+        <p className="self-end">{date.toString()}</p>
+        <h1>{title}</h1>
+        <h2>{description}</h2>
+        <div className="border-2 my-4 border-lime-400"></div>
+      </section>
+      <MarkdownViewer content={content} />
+    </article>
+  );
+}
