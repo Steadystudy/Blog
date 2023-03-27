4d11bc163d331ad68d88bf6ab92f0d143c4bdf17
Steadystudy <sgmin206@naver.com>
2023-03-27 15:42:47 +0900

    Feat: MarkdownViewer

---
+++ src/components/MarkdownViewer.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import Image from 'next/image';
+
+interface Props {
+  content: string;
+}
+
+export default function MarkdownViewer({ content }: Props) {
+  return (
+    <ReactMarkdown
+      className="prose xl:prose-xl max-w-none"
+      components={{
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || '');
+          return !inline && match ? (
+            <SyntaxHighlighter language={match[1]} PreTag="div" {...props} style={materialDark}>
+              {String(children).replace(/\n$/, '')}
+            </SyntaxHighlighter>
+          ) : (
+            <code className={className} {...props}>
+              {children}
+            </code>
+          );
+        },
+        img: (image) => (
+          <Image
+            className="w-full max-h-96"
+            src={image.src || ''}
+            alt={image.alt || ''}
+            width={500}
+            height={500}
+          />
+        ),
+      }}
+    >
+      {content}
+    </ReactMarkdown>
+  );
+}
