5844f99001eacce3c5c958b23da3cd60691d2821
Steadystudy <sgmin206@naver.com>
2023-03-28 22:44:39 +0900

    Feat: Contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,25 @@
+import ContactForm from '@/components/ContactForm';
+
+const LINKS = [
+  { image: 'Github Emoji', url: '' },
+  { image: 'Linked In Emoji', url: '' },
+];
+
 export default function ContactPage() {
-  return <div>ContactPage</div>;
+  return (
+    <>
+      <section>
+        <h1>Contact Me</h1>
+        <p>sgmin206@naver.com</p>
+        <div>
+          {LINKS.map((link, i) => (
+            <a key={i} href={link.url} target="_blank" rel="noreferrer">
+              {link.image}
+            </a>
+          ))}
+        </div>
+      </section>
+      <ContactForm />
+    </>
+  );
 }
