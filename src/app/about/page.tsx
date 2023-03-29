36510a2e659a07b86d18b6625ddb93b10024c746
Steadystudy <sgmin206@naver.com>
2023-03-29 21:05:43 +0900

    Feat: about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,15 @@
+import Introduction from '@/components/Introduction';
+
 export default function AboutPage() {
-  return <div>AboutPage</div>;
+  return (
+    <>
+      <Introduction />
+      <section className="bg-gray-100">
+        <h1>Who Am I?</h1>
+        <p>풀스택을 목표로 하는 프론트엔드 개발자</p>
+        <h1>Skills</h1>
+        <p>React, Next, Typescript, Git</p>
+      </section>
+    </>
+  );
 }
