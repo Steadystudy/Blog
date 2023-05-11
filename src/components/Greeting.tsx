e26180ef99e3e384320dd6d83cc75a3a095e27c9
Steadystudy <sgmin206@naver.com>
2023-05-11 23:10:30 +0900

    Feat: Greeting(인삿말)

---
+++ src/components/Greeting.tsx
@@ -0,0 +1,15 @@
+import RotatingText from './RotatingText';
+
+export default function Greeting() {
+  return (
+    <div>
+      <h1>
+        안녕하세요👋
+        <br />
+        저는 <RotatingText />
+      </h1>
+      <h2>프론트엔드 개발자 민상기입니다.</h2>
+      <h3>꾸준히 코딩하는 사람</h3>
+    </div>
+  );
+}
