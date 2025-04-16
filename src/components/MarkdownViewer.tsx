64371459ac7ed9f2951e7103dc32c888c24529e0
Steadystudy <sgmin206@naver.com>
2025-04-17 00:55:45 +0900

    Feat: html 태그로 마크 다운 추가

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+import ReactMarkdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import Image from 'next/image';
 import remarkGfm from 'remark-gfm';
+import rehypeRaw from 'rehype-raw';
 
 interface Props {
   content: string;
@@ -12,44 +13,46 @@ interface Props {
 
 export default function MarkdownViewer({ content }: Props) {
   return (
-    <ReactMarkdown
-      className="mb-8 prose xl:prose-xl max-w-none dark:prose-invert dark:prose-headings:text-green-100"
-      components={{
-        code({ node, inline, className, children, ...props }) {
-          const match = /language-(\w+)/.exec(className || '');
-          return !inline && match ? (
-            <SyntaxHighlighter language={match[1]} PreTag="div" {...props} style={materialDark}>
-              {String(children).replace(/\n$/, '')}
-            </SyntaxHighlighter>
-          ) : (
-            <code className={`dark:text-green-100`} {...props}>
+    <div className="mb-8 prose xl:prose-xl max-w-none dark:prose-invert dark:prose-headings:text-green-100">
+      <ReactMarkdown
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match ? (
+              <SyntaxHighlighter language={match[1]} PreTag="div" {...props} style={materialDark}>
+                {String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={`dark:text-green-100`} {...props}>
+                {children}
+              </code>
+            );
+          },
+          img: (image) => (
+            <Image
+              className="object-cover w-full"
+              src={image.src || ''}
+              alt={image.alt || ''}
+              width={500}
+              height={500}
+            />
+          ),
+          details: ({ children, ...props }) => (
+            <details className="my-4" {...props}>
               {children}
-            </code>
-          );
-        },
-        img: (image) => (
-          <Image
-            className="object-cover w-full"
-            src={image.src || ''}
-            alt={image.alt || ''}
-            width={500}
-            height={500}
-          />
-        ),
-        details: ({ children, ...props }) => (
-          <details className="my-4" {...props}>
-            {children}
-          </details>
-        ),
-        summary: ({ children, ...props }) => (
-          <summary className="cursor-pointer hover:text-primary" {...props}>
-            {children}
-          </summary>
-        ),
-      }}
-      remarkPlugins={[remarkGfm]}
-    >
-      {content}
-    </ReactMarkdown>
+            </details>
+          ),
+          summary: ({ children, ...props }) => (
+            <summary className="cursor-pointer hover:text-primary" {...props}>
+              {children}
+            </summary>
+          ),
+        }}
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw]}
+      >
+        {content}
+      </ReactMarkdown>
+    </div>
   );
 }
