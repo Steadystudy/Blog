0a2069d8175440e36073683c5400c7d37a106e3b
Steadystudy <sgmin206@naver.com>
2025-06-17 20:06:41 +0900

    style: sidebar menubox 수정

--- src/components/Sidebar/MenuBox.tsx
+++ src/components/Sidebar/MenuBox.tsx
@@ -9,7 +9,7 @@ export default function MenuBox({ children, border, onClick }: Props) {
     <div
       onClick={onClick}
       className={`w-[60px] h-[60px] text-[25px] lg:w-[80px] lg:h-[80px] flex justify-center items-center p-2 hover:bg-accent text-white lg:text-[30px] cursor-pointer
-        ${border && 'border-b-4  border-pink-300 lg:border-r-4 lg:border-b-0'}
+        ${border && 'border-b-4  border-accent lg:border-r-4 lg:border-b-0'}
       `}
     >
       {children}
